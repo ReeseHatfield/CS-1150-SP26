80cc17c96582404100c9d7d7b5c4c115fa871842
ReeseHatfield <reesehatfield02@gmail.com>
2026-03-18 15:59:36 -0400

    mar 18

---
+++ Javascript/grading.py
@@ -0,0 +1,17 @@
+
+
+# Ask user how tall they are
+height = input("How tall are you? ")
+
+height = int(height)
+
+if height > 6:
+    print("You are tall!")
+else:
+    print("You are short")
+
+# Read in data
+# If user is taller than 6 ft
+# Tell them they are tall
+# Else
+# Tell them they are short
